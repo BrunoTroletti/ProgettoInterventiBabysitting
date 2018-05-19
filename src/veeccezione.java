42030f880417ef174ce9906f806d4a6dd6c15e99
Luca <bruno.troletti@gmail.com>
2018-05-19 09:46:48 +0200

    terzo commit

---
+++ src/veeccezione.java
@@ -0,0 +1,15 @@
+
+public class veeccezione 
+
+{
+private String message;
+	
+	public FileException (String message)
+	{
+		this.message = message;
+	}
+	public String toString ()
+	{
+		return message;
+	}
+}
